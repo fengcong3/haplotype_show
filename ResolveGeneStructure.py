862c6539340d1c59c878bc8a6d4db8fe7d3507e9
fengcong33 <congfeng97@gmail.com>
2020-12-28 18:03:52 +0800

    update example json file and ResolveGeneStructure.py

--- ResolveGeneStructure.py
+++ ResolveGeneStructure.py
@@ -15,7 +15,48 @@ Rscript="/public/home/fengcong/anaconda2/envs/R/bin/Rscript"
 py3="/public/home/fengcong/anaconda2/envs/py3/bin/python"
 
 def gene_structure(gene_name,gff_file):
-    pass
+    '''
+    "gene_structure":{
+        "gene":[[1,8200]],
+        "gene_name":"TraesCS1A02G000100.1",
+        "upstream":[[1,2000]],
+        "5p_UTR":[[2001,2050]],
+        "exon":[[2051,3400],[5100,5700],[7300,8000]],
+        "intron":[[3401,5099],[5701,7299]],
+        "3p_UTR":[[8001,8200]]
+    }
+    '''
+    gene_structure = {
+        "gene":[],
+        "gene_name":"",
+        "upstream":[],
+        "5p_UTR":[],
+        "exon":[],
+        "intron":[],
+        "3p_UTR":[]
+    }
+    #get gene information
+    info=[]
+    inf = open(gff_file,"r")
+    for line in  inf.readlines():
+        if line.find(gene_name.split(".")[0]) != -1:
+            info.append(line.strip())
+    inf.close()
+
+    #deal with the information 
+    if len(info) == 0:
+        sys.stderr.write("can't find this gene in gff file\n")
+        exit(-1)
+    else:
+        for line in info:
+            ls = line.split()
+            if ls[2] == "gene":
+                gene_structure["gene"].append([int(ls[3]),int(ls[4])])
+            else:
+                if ls[2] =="mRNA":
+                    pass
+                ##unfinished 
+
 
 
 if __name__ == "__main__":
@@ -40,7 +81,7 @@ if __name__ == "__main__":
 
 
     ##deal with the args
-    gene_name = args.gene 
+    gene_name = args.gene if args.gene.find(".") != -1 else args.gene+".1" 
     gff_file = args.gff
     snp_file = args.SNP
     sv_file = args.sv    #can be ignore
@@ -48,3 +89,5 @@ if __name__ == "__main__":
     inf_file = args.info
 
     ##gene_structure
+    
+
